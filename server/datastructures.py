ceafef14b153bb24119735147fc99706e0bc8b88
Capstone Group 15 <g15@ece-e1-465-n.eng.umanitoba.ca>
2020-01-10 13:56:14 -0600

    Updated server to handle all the produced datatypes, not just temperature

--- server/datastructures.py
+++ server/datastructures.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from dataclasses import dataclass, asdict
 from datetime import datetime
 from enum import Enum
@@ -7,6 +8,22 @@ _subclasses = {}
 
 class DatumType(Enum):
     TEMPERATURE = 'temperature'
+    CO2 = 'co2'
+    HUMIDITY = 'humidity'
+    LUX = 'lux'
+    ACCELERATION = 'acceleration'
+    TVOC = 'tvoc'
+
+
+class DatumUnits(Enum):
+    PartsPerMillion = 'ppm'
+    PartsPerBillion = 'ppb'
+    Lux = 'lux'
+    microGs = 'μG'
+    milliGs = 'mG'
+    Gs = 'G'
+    degreesC = '°C'
+    percent = '%'
 
 
 class TimeRange:
@@ -29,12 +46,14 @@ class Datum:
     time: datetime
     sensor_id: str
     type: DatumType
+    units: DatumUnits
 
-    def __init__(self, value, time: str, sensor_id: str, type: DatumType):
+    def __init__(self, value, time: str, sensor_id: str, type: DatumType, units: DatumUnits):
         self.value = value
         self.time = datetime.fromisoformat(time)
         self.sensor_id = sensor_id
         self.type = type
+        self.units = units
 
     def __eq__(self, other):
         return isinstance(other, Datum) and other.value == self.value
@@ -47,6 +66,7 @@ class Datum:
         ret = asdict(self)
         ret['time'] = ret['time'].isoformat()
         ret['type'] = ret['type'].value
+        ret['units'] = ret['units'].value
         return ret
 
     @staticmethod
@@ -57,6 +77,7 @@ class Datum:
     def from_dict(cls, data):
         data.pop('_id', None)
         typ = data.pop('type')
+        units = data.pop('units')
         klass = _subclasses.get(typ, cls)
         return klass(**data)
 
@@ -64,4 +85,54 @@ class Datum:
 @dataclass()
 class Temperature(Datum, typ=DatumType.TEMPERATURE):
     def __init__(self, value, time, sensor_id):
-        super().__init__(value, time, sensor_id, DatumType.TEMPERATURE)
+        super().__init__(
+            value,
+            time,
+            sensor_id,
+            DatumType.TEMPERATURE,
+            DatumUnits.degreesC)
+
+
+@dataclass()
+class CO2(Datum, typ=DatumType.CO2):
+    def __init__(self, value, time, sensor_id):
+        super().__init__(value, time, sensor_id, DatumType.CO2, DatumUnits.PartsPerMillion)
+
+
+@dataclass()
+class Humidity(Datum, typ=DatumType.HUMIDITY):
+    def __init__(self, value, time, sensor_id):
+        super().__init__(value, time, sensor_id, DatumType.HUMIDITY, DatumUnits.percent)
+
+
+@dataclass()
+class Lux(Datum, typ=DatumType.LUX):
+    def __init__(self, value, time, sensor_id):
+        super().__init__(value, time, sensor_id, DatumType.LUX, DatumUnits.Lux)
+
+
+@dataclass()
+class TVOC(Datum, typ=DatumType.TVOC):
+    def __init__(self, value, time, sensor_id):
+        super().__init__(value, time, sensor_id, DatumType.TVOC, DatumUnits.PartsPerBillion)
+
+
+AccelerationData = namedtuple('AccelerationData', ['xdata', 'ydata', 'zdata'])
+
+
+@dataclass()
+class Acceleration(Datum, typ=DatumType.ACCELERATION):
+    value: AccelerationData
+
+    def __init__(self, xdata, ydata, zdata, time, sensor_id):
+        value = AccelerationData(xdata, ydata, zdata)
+        super().__init__('', time, sensor_id, DatumType.ACCELERATION, DatumUnits.microGs)
+        self.value = value
+
+    def to_dict(self):
+        ret = asdict(self)
+        ret['time'] = ret['time'].isoformat()
+        ret['type'] = ret['type'].value
+        ret['units'] = ret['units'].value
+        ret['xdata'], ret['ydata'], ret['zdata'] = ret.pop('value')
+        return ret
